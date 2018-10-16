45065d1788549036f1940ee2c65b21a01511048d
BrunoMartins <sgtbrunoalmeida@gmail.com>
2018-10-15 21:11:17 -0300

    Trabalho v1.0

--- Filial.java
+++ Filial.java
@@ -2,7 +2,7 @@ import java.rmi.Naming;
 public class Filial{
   public static void main(String[] args){
     try{
-      Matrix c = (Matrix) Naming.lookup("rmi://192.168.0.100/Matrix");
+      Matrix c = (Matrix) Naming.lookup("rmi://192.168.0.5/MatrixService");
       System.out.println("Adicao : "+c.add(10,15));
     }catch (Exception e){
       e.printStackTrace();
